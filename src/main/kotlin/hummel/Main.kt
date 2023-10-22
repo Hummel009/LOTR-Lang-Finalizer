88f911a9ad1a7b7659191ab6167da79f77de26f7
Hummel009 <hummel.turbamentum@gmail.com>
2023-10-22 14:06:07 +0300

    release

---
+++ src/main/kotlin/hummel/Main.kt
@@ -0,0 +1,23 @@
+package hummel
+
+import java.io.File
+
+fun main(args: Array<String>) {
+	if (args.size < 2) {
+		println("Usage: java -jar ThisApp.jar langUnfinalized.lang langFinalized.lang")
+		return
+	}
+
+	val firstFile = File(args[0])
+	val lines = File(args[0]).readLines()
+
+	val uniqueLines = lines.map {
+		it.split('=', limit = 2)
+	}.distinctBy {
+		it[0]
+	}.map {
+		it.joinToString("=")
+	}
+
+	File("${firstFile.nameWithoutExtension}-finalized.lang").writeText(uniqueLines.joinToString("\n"))
+}
